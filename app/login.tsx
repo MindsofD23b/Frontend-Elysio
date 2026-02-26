67c72a9fd90f3a5603de725a29137a65cbdafef9
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-02-26 22:04:33 +0100

    [feat] Added button comp for easy access (#68)

--- app/login.tsx
+++ app/login.tsx
@@ -2,26 +2,54 @@ import Button from "@/components/button";
 import { Link } from "expo-router";
 import { HomeIcon } from "lucide-react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { useTheme } from "./theme/context";
+import { colors } from "./theme/theme";
 
 export default function Login() {
-    return (
-        <View style={styles.container}>
-            <HomeIcon size={48} color="#EC136A" />
-            <Text>Elysio</Text>
+    const { theme, gs } = useTheme();
 
-            <Text>Login to continue</Text>
+    return (
+        <View style={gs.container}>
+            <HomeIcon size={48} color={theme.accent} />
+            <Text style={gs.h1}>Elysio</Text>
+
+            <Text style={gs.h2}>Login to continue</Text>
             <Button onPress={() => alert("Login button pressed")}>Continue with Email</Button>
             <Button onPress={() => alert("Login button pressed")}>Continue with Phone Number</Button>
 
-            <Link href="/register">Don`t have an account? Register</Link>
+            <Link href="/register"><Text style={gs.btnText}>Don&apos;t have an account? Register</Text></Link>
+
+            <View>
+                <View></View>
+                <View><Text style={gs.bodyText}>or</Text></View>
+                <View></View>
+            </View>
+
+
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-});
+export const makeStyles = (theme: typeof colors.light) =>
+    StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: theme.background,
+        },
+        button: {
+            backgroundColor: theme.primary,
+            padding: 10,
+            borderRadius: 5,
+            alignItems: "center",
+        },
+        buttonText: {
+            color: theme.text,
+            fontSize: 16,
+            fontWeight: "bold",
+        },
+        text: {
+            color: theme.text,
+        }
+    });
