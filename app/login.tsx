ebd0258ba1f3daad3bc183063da00adaed5351f7
Jamie-Poeffel <Jamie.poeffel@gmail.com>
2026-02-26 17:17:03 +0100

    [feat] Login data (#68)

--- app/login.tsx
+++ app/login.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import Button from "@/components/button";
+import { Link } from "expo-router";
+import { HomeIcon } from "lucide-react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+export default function Login() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Edit app/login.tsx to edit this screen.</Text>
+        <View style={styles.container}>
+            <HomeIcon size={48} color="#EC136A" />
+            <Text>Elysio</Text>
+
+            <Text>Login to continue</Text>
+            <Button onPress={() => alert("Login button pressed")}>Continue with Email</Button>
+            <Button onPress={() => alert("Login button pressed")}>Continue with Phone Number</Button>
+
+            <Link href="/register">Don`t have an account? Register</Link>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
---
+++ components/button.tsx
@@ -0,0 +1,28 @@
+import { Pressable, StyleSheet, Text } from "react-native";
+
+interface ButtonProps {
+    children: React.ReactNode;
+    onPress: () => void;
+}
+
+export default function Button({ children, onPress }: ButtonProps) {
+    return (
+        <Pressable onPress={onPress} style={styles.button}>
+            <Text style={styles.buttonText}>{children}</Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: "#EC136A",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+});
