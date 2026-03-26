91406cd6e3ba615de67493e38a85c570b53e4dc3
Luis Allamand <Luis@g-12.ch>
2026-03-26 21:41:10 +0100

    feat: better layout for Videocall

--- i18n.ts
+++ i18n.ts
@@ -1,4 +1,4 @@
-import { getLocales } from "expo-localization";
+// import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 // all language files made by claud ai
 import en from "./locales/en.json";
@@ -12,7 +12,9 @@ const i18n = new I18n({
     es,
 });
 
-i18n.locale = getLocales()[0]?.languageCode ?? "en";
+// i18n.locale = getLocales()[0]?.languageCode ?? "en";
+i18n.locale = "en";
+i18n.defaultLocale = "en";
 i18n.enableFallback = true;
 
 export default i18n;
